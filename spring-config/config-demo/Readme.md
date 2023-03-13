204a215bf0ada9843797a7cb109e77d2c2b77a7a
Yevhen Skyba <evad.se.dev@gmail.com>
2023-03-13 13:16:35 +0200

    test 2 controllers

--- spring-config/config-demo/Readme.md
+++ spring-config/config-demo/Readme.md
@@ -7,3 +7,13 @@
 ```http
 http://localhost:8080/config_demo_war/
 ```
+
+## controllers 
+base url in debug settings http://localhost:8080/
+context is empty 
+
+    * FirstController (http://localhost:8080/first/hello)
+    * SecondController
+
+Default tomcat debug settings
+http://localhost:8080/config_demo_war/first/hello
--- spring-config/config-demo/src/main/java/com/se/sample/controllers/SecondController.java
+++ spring-config/config-demo/src/main/java/com/se/sample/controllers/SecondController.java
@@ -1,4 +1,4 @@
-package com.se.sample;
+package com.se.sample.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Neil Alishev
  */
 @Controller
-public class HelloController {
+public class SecondController {
 
-    @GetMapping("/hello-world")
-    public String sayHello() {
-        return "hello_world";
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
     }
 }
