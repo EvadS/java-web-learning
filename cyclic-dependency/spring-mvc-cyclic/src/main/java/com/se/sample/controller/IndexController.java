51e4707de5d7672e80f91d52e20cb3929fcc0217
Yevhen Skyba <evad.se.dev@gmail.com>
2023-03-13 15:02:39 +0200

    LOG4J2 integrated

--- cyclic-dependency/spring-mvc-cyclic/src/main/java/com/se/sample/controller/IndexController.java
+++ cyclic-dependency/spring-mvc-cyclic/src/main/java/com/se/sample/controller/IndexController.java
@@ -2,27 +2,29 @@ package com.se.sample.controller;
 
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.PostConstruct;
 
-import org.apache.log4j.Logger;
-
 @RestController
 @RequestMapping("/test")
 public class IndexController {
 
-    private static final Logger logger = Logger.getLogger(IndexController.class);
+    static final Logger logger = LogManager.getLogger(IndexController.class.getName());
+
+
 
     @GetMapping
     public String getHello() {
-        logger.info("* get hello request");
+        logger.info("+++++ get hello request");
         return "Hello world!!1";
     }
 
     @PostConstruct
     private void init(){
-        logger.info("** IndexController created !! ");
+      //  logger.info("** IndexController created !! ");
     }
 }
